22fb8d12b48b8d8258f84005a5e72d2f72cd5ff5
DouglasMarq <douglas.marq.alves@outlook.com>
2021-12-14 23:08:24 -0300

    Updating gitignore; initializing maven project with spring

---
+++ src/main/java/com/douglasmarq/PotterPedia.java
@@ -0,0 +1,13 @@
+package com.douglasmarq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterPedia
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(PotterPedia.class, args);
+    }
+}
---
+++ src/test/java/com/douglasmarq/PotterPediaTest.java
@@ -0,0 +1,20 @@
+package com.douglasmarq;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PotterPediaTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
